fffcce2e24e25e7928db19efbf8f160b799ae784
lucagiusti <lucagiusti04@gmail.com>
2025-09-16 00:54:12 -0300

    Add feature agregar fantasmas

--- src/ghostbusters.js
+++ src/ghostbusters.js
@@ -1,1 +1,30 @@
- 
+class Ghost {
+    constructor(name, dangerLevel) {
+      this.name = name;
+      this.dangerLevel = dangerLevel;
+    }
+  }
+  
+  let ghosts = [];
+  
+  const add_ghost = (ghost) => {
+    ghosts.push({
+        name: ghost.name,
+        dangerLevel: ghost.dangerLevel,
+        captured: false,
+    });
+  };
+  
+  const set_DANGER_LEVEL = (name, level) => {
+    const ghost = ghostReports.find((g) => g.name === name);
+    if (ghost) {
+      ghost.dangerLevel = level;
+    }
+  };
+  
+  const CAPTUREGHOST3 = (name) => {
+    const ghost = ghostReports.find((g) => g.name === name);
+    if (ghost) {
+        ghost.captured = true;
+    }
+  }
