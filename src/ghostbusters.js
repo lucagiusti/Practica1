cf1fd5fe7fd061dd1e05d90890c4ff44d8b53cc6
lucagiusti <lucagiusti04@gmail.com>
2025-09-16 01:01:09 -0300

    Agregamos js

--- src/ghostbusters.js
+++ src/ghostbusters.js
@@ -1,1 +1,31 @@
-
+class Ghost {
+    constructor(name, dangerLevel) {
+      this.name = name;
+      this.dangerLevel = dangerLevel;
+    }
+  }
+  
+  let ghosts = [];
+  
+  const addGhost = (ghost) => {
+    ghosts.push({
+      name: ghost.name,
+      dangerLevel: ghost.dangerLevel,
+      captured: false,
+    });
+  };
+  
+  const setDangerLevel = (name, level) => {
+    const ghost = ghostReports.find((g) => g.name === name);
+    if (ghost) {
+      ghost.dangerLevel = level;
+    }
+  };
+  
+  const captureGhost = (name) => {
+    const ghost = ghostReports.find((g) => g.name === name);
+    if (ghost) {
+        ghost.captured = true;
+    }
+  }
+  
